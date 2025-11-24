430e40000a47fb2a0d3d8b60f300cd0af2f4eebc
Mathieu <mathieu.mjr@gmail.com>
2025-11-24 15:49:51 +0100

    function that turns float to str with python annotation

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains a function
+that typecast a float into str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    return an str of a float
+    """
+    return str(n)
