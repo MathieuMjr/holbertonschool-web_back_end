17f629232ea23bb0f63748c5a12ccaf948042060
Mathieu <mathieu.mjr@gmail.com>
2025-11-24 16:59:35 +0100

    used union to please the che-cker

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,10 +4,10 @@ This module contains a function
 that make the sum of floats in
 a list
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Function that makes the sum
     of floats in a list
