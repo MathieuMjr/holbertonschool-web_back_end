5cf59cc13950c5d79b541e7db6c848e6661fbac2
Mathieu <mathieu.mjr@gmail.com>
2025-11-24 15:25:59 +0100

    function that concat 2 strings with python annotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains a function
+that does the concatenation of
+two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function concats two strings"""
+    return str1 + str2
