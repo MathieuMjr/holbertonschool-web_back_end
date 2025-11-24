e8cad43bdc3147ddfcb18820ad977de2470a3847
Mathieu <mathieu.mjr@gmail.com>
2025-11-24 15:19:57 +0100

    function add with typing

---
+++ 0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module contains a function
+that at two int and return a float
+"""
+
+
+def add(a: int, b: int) -> float:
+    """This function does the sum of two int"""
+    return a + b
