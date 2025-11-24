99f98e0a04eaa6cd10dc2489735ee053f5376d73
Mathieu <mathieu.mjr@gmail.com>
2025-11-24 16:58:10 +0100

    function /w Python annotation that makes sum a of a mixed types list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains a function
+that make the sum of floats in
+a list
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+    """
+    Function that makes the sum
+    of floats in a list
+    """
+    res: float = 0
+    for n in mxd_lst:
+        res += n
+    return res
