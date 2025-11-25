e611c99a9ef5e406643b742862905e242629f69d
Mathieu <mathieu.mjr@gmail.com>
2025-11-25 10:27:17 +0100

    function that returns another function with python annotation

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns
+another function.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: int) -> Callable[[float], float]:
+    """
+    Function that takes a multiplier and return
+    a function that multiply this multiplier to a
+    float
+    """
+    def multiply(number: float) -> float:
+        return number * multiplier
+    return multiply
+# On indique un callable en retour avec en liste, ses types de paramètres
+# entre crochets et son type de retour Callable[[type_arg_1, type_arg_2],
+# type_retour] ensuite dans la définition de la 1ère on définit la seconde,
+# ses args, son retour et enfin, on met le retour de la fonction 'parente'
+# qui retourne la fonction enfant. Callable fait le lien
