3b158e90a3b8b7869bbd859056224fe5391a90f1
Mathieu <mathieu.mjr@gmail.com>
2025-11-25 12:05:38 +0100

    function /w python annotation of comprehension that returns a tuple of iterable and len of iterable from a sequence

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns
+tuples with iterable and iterable lenght
+from a sequence
+"""
+from typing import Sequence, Any, Iterable, Tuple, List
+
+def element_length(lst: Sequence[Iterable[Any]]) -> List[Tuple[Iterable[Any]], int]:
+    """
+    This function returns tuples of an Iterable
+    and it's length from a sequence
+    """
+    return [(i, len(i)) for i in lst]
