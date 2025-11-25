371a32aa754c626eeef6a5a0224d8ffa78214d2d
Mathieu <mathieu.mjr@gmail.com>
2025-11-25 12:14:43 +0100

    fixed the annotations

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,12 +4,10 @@ This module contains a function that returns
 tuples with iterable and iterable lenght
 from a sequence
 """
-from typing import Sequence, Any, Iterable, Tuple, List
+from typing import Sequence, Iterable, Tuple, List
 
 
-def element_length(lst: Sequence[Iterable[Any]]) -> List[
-    Tuple[Iterable[Any]], int
-        ]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     This function returns tuples of an Iterable
     and it's length from a sequence
