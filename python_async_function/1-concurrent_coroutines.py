900d15711b0934fa78f2db4d0d31b6886af14a1f
Mathieu <mathieu.mjr@gmail.com>
2025-11-26 12:12:00 +0100

    fixed so tasks are run concurently !

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,6 +4,7 @@ Docstring pour python_async_function.1-concurrent_coroutines
 Contains a function that perform n time wait_random
 with a specific maximum delay
 """
+import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -19,7 +20,11 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     :return: The list of delays
     :rtype: List[float]
     """
-    new_list = []
+    delays_list = []
+    tasks = []
     for i in range(0, n):
-        new_list.append(await wait_random(max_delay))
-    return new_list
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+    for completed_task in asyncio.as_completed(tasks):
+        delays_list.append(await completed_task)
+    delays_list.sort()
+    return delays_list
