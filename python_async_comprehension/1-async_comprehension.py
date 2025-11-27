5e226067a210f412bbcac96ad2f5ba77bb207c6b
Mathieu <mathieu.mjr@gmail.com>
2025-11-27 11:17:33 +0100

    function that consume generator to return result

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Docstring pour python_async_comprehension.1-async_comprehension
+"""
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """
+    Docstring pour async_comprehension
+    """
+    result = [i async for i in async_generator()]
+    return (result)
