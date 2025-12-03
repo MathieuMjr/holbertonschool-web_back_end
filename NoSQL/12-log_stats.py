498d372e267124b71d55ade6258a47d057b2a592
Mathieu <mathieu.mjr@gmail.com>
2025-12-03 16:41:36 +0100

    boum

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
+"""Write a Python script that provides some stats about Nginx logs stored in MongoDB:"""
 
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
 
-    print(f'{logs_collection.count_documents({})} logs')
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    collection = client.logs.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
     print("Methods:")
-    get = logs_collection.count_documents({'method': 'GET'})
-    post = logs_collection.count_documents({'method': 'POST'})
-    put = logs_collection.count_documents({'method': 'PUT'})
-    patch = logs_collection.count_documents({'method': 'PATCH'})
-    delete = logs_collection.count_documents({'method': 'DELETE'})
-    status = logs_collection.count_documents(
-        {'method': 'GET',
-         'path': '/status'})
-    print(f'\tmethod GET: {get}')
-    print(repr("\tmethod GET"))
-    print("\t" + "method POST: " + str(post))
-    print("\t" + "method PUT: " + str(put))
-    print("\t" + "method PATCH: " + str(patch))
-    print("\t" + "method DELETE: " + str(delete))
-    print(f'{status} status check')
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
