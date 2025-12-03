2362c13ea5fafc469c69d865edf9c964fefa9413
Mathieu <mathieu.mjr@gmail.com>
2025-12-03 11:46:13 +0100

    function that  insert one data

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Docstring pour NoSQL.9-insert_school
+"""
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Docstring pour insert_school
+    
+    :param mongo_collection: Mongo collection
+    :param kwargs: Datas as a dict
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
