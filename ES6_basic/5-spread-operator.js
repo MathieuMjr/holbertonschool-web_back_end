6e0e081b1350d25ca42bffbfc65d3a741313a36e
Mathieu <mathieu.mjr@gmail.com>
2025-12-09 11:06:38 +0100

    only one line

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const newArray = array1.concat(array2, Array.from(string));
-  return newArray;
+  return array1.concat(array2, Array.from(string));
 }
