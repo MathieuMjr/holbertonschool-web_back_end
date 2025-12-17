c10d5c8b6fed836dfbd7b0d87e0898b0fbc04895
Mathieu <mathieu.mjr@gmail.com>
2025-12-17 16:28:15 +0100

    function that treat several promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import {uploadPhoto, createUser} from './utils'
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  photo.then(result1 => createUser().then(result2 => {
+    console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`)
+  }))
+  .catch(() => console.log('Signup system offline'))
+}
+// use Promise.all instead
