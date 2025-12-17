5bfcd55ac63eb5996ddbeee69fbd0b6825f9e40b
Mathieu <mathieu.mjr@gmail.com>
2025-12-17 15:24:33 +0100

    fixed object return

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     console.log('Got a response from the API');
-    return {'statuts': 200, 'body': 'success'};
+    return {'statuts': 200, 'body': success};
   })
   .catch(() => {
     console.log('Got a response from the API');
