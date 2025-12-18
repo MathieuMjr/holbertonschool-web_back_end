9624efbe5bc4c28bb4ff4b029dea61342f3cd160
Mathieu <mathieu.mjr@gmail.com>
2025-12-18 12:26:44 +0100

    main given to test my function

--- ES6_promise/6-main.js
+++ ES6_promise/6-main.js
@@ -1,3 +1,7 @@
 import handleProfileSignup from './6-final-user';
 
 console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+handleProfileSignup("John", "Doe", "Gerald.jpg")
+  .then(result => {
+    console.log(result);
+  });
