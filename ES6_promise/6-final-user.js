af1a3875813cf77c8adf2f52880604f5e3b070ce
Mathieu <mathieu.mjr@gmail.com>
2025-12-18 12:27:11 +0100

    fixed : specific return

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,7 +3,12 @@ import uploadPhoto from './5-photo-reject'
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises);
+  return Promise.allSettled(promises).then(results => results.map(result => {
+    if (result.status === 'fulfilled') {
+      return {'status': result.status, 'value': result.value}
+    }
+    return {'status': result.status, 'value': result.reason.toString()}
+  }))
 }
 
 // allSettled retourne... une promesse !
