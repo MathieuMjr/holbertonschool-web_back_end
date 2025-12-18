ec56c047c2c28a41fd49def140f7aa6e74e55fe8
Mathieu <mathieu.mjr@gmail.com>
2025-12-18 11:25:01 +0100

    allSettled

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises);
+}
+
+// allSettled retourne... une promesse !
+// A t0 quand le programme s'execute, allSettled est pending : pas encore résolu
+// Les deux promesses à l'intérieure vont se résoudre ou se rejeter. 
+// allSettled passera résolu avec en valeur un array contenant le statut
+// et la valeur de chaque promesse. 
+// Une promesse est donc retournée, et sa valeur est automatiquement,
+// car c'est ainsi que fonction allSettled, un array de résultat de promesses.
