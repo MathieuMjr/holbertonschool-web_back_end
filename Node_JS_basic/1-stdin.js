dbec1f10757394b06bd7758677fdbdd95822971f
Mathieu <mathieu.mjr@gmail.com>
2026-01-05 15:43:18 +0100

    fixed coding style

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', data => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
-})
-process.on('exit', function() {
+});
+process.on('exit', () => {
   console.log('This important software is now closing');
-})
+});
