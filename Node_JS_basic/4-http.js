635dc1700467f2e2f0e9bb2a4c276f0171878fe7
Mathieu <mathieu.mjr@gmail.com>
2026-01-06 17:06:07 +0100

    named the server 'app'

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,12 +3,14 @@ const { createServer } = require('node:http');
 const hostname = '127.0.0.1';
 const port = '1245';
 
-const server = createServer((req, res) => {
+const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname, () => {
+module.exports = app;
+
+app.listen(port, hostname, () => {
   console.log('Coucou les roudoudoux !');
 });
