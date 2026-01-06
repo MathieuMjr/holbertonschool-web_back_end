85ee25f7e3ff413e35f21e4a1b37fd3d06e0a159
Mathieu <mathieu.mjr@gmail.com>
2026-01-06 16:19:09 +0100

    async reading file with promises @o@

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,37 @@
 const fs = require('fs');
-const { parse } = require('csv-parse');
-// accolades importantes pour récup la fonction parse
-// console.log(parse); pour voir avec et sans accolades
 
 function countStudents(path) {
-  try {
-    let totalStudents = 0;
-    const results = {};
-
-    const data = fs.readFile(path, 'utf8');
-    const records = parse(data, { columns: true, skip_empty_lines: true });
-
-    for (const row of records) {
-      totalStudents += 1;
-      if (!(row.field in results)) {
-        results[row.field] = { students_nb: 1, students_list: [row.firstname] };
-        // single quote pas obligatoire pour les clés en JS si pas de char spéciaux ou nb.
-      } else {
-        results[row.field].students_nb += 1;
-        results[row.field].students_list.push(row.firstname);
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', ((err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
-    }
-    console.log(`Number of students: ${totalStudents}`);
-    for (const [key, value] of Object.entries(results)) {
-      console.log(`Number of students in ${key}: ${value.students_nb}. List: ${value.students_list.join(', ')}`);
-    }
-  } catch (error) {
-    console.log('Cannot load the database');
-  }
+      let totalStudents = 0;
+      const results = {};
+
+      const lines = data.split('\n'); // le fichier devient un tableau de ligne en se basant sur \n pour répartir les lignes
+      const filteredLines = lines.filter((line) => line.trim() !== ''); // retirer les lignes vides
+      const studentLines = filteredLines.slice(1);
+      // on lit le tableau de ligne à partir de l'index 1 (sans les entêtes)
+
+      for (const line of studentLines) { // pour chaque ligne...
+        totalStudents += 1;
+        const content = line.split(','); // va faire un tableau en séparant les valeurs via la ','
+        if (!(content[3] in results)) {
+          results[content[3]] = { students_nb: 1, students_list: [content[0]] };
+        } else {
+          results[content[3]].students_nb += 1;
+          results[content[3]].students_list.push(content[0]);
+        }
+      }
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [key, value] of Object.entries(results)) {
+        console.log(`Number of students in ${key}: ${value.students_nb}. List: ${value.students_list.join(', ')}`);
+      }
+      resolve();
+    }));
+  });
 }
 
 module.exports = countStudents;
