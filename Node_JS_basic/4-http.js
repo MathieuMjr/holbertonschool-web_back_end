145126e1ebf0733fa3d8e6d2d5da29d55dee27cd
Mathieu <mathieu.mjr@gmail.com>
2026-01-06 16:54:28 +0100

    small http server

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = '1245';
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+server.listen(port, hostname, () => {
+  console.log('Coucou les roudoudoux !');
+});
